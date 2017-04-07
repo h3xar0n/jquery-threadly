042c830b7cf6186de9a11adde6bfa3d1700a82bd
h3xar0n <aron@glambyadelina.com>
2017-04-07 16:23:04 -0400

    Comments now post

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var main = function() {
   $('form').submit(function() {
-    
+    var comment = $('#comment').val();  
     if(comment !== "") {
       var html = $('<li>').text(comment);
-      
+      $(html).prependTo('.comments'); 
     }
 
     return false;
